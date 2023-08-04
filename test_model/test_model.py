51f598b3c3ff415ec2eea63cee2a62e786a3dcdb
hf2503 <hfmole@hotmail.com>
2023-08-04 15:57:36 +0200

    test overlap

--- test_model/test_model.py
+++ test_model/test_model.py
@@ -1,8 +1,21 @@
-import pytest
-import requests
-from fastapi.testclient import TestClient
-from api import app
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from requests.auth import HTTPBasicAuth
+import pandas as pd
+import sys
+sys.path.append('../preprocessing')
+sys.path.append('ml-20m')
+import os
+
+from utils import *
+from create_data import Data
 
 
+def test_check_overlap():
+    df=Data(min_relevance=0.85,min_rating=4000).data
+    train_data, test_data, title_dict = split_data_random(df)
+    assert check_overlap(train_data,test_data) == True
+
+
+#test_check_overlap()
+
+print("ok")
+print(sys.path)
+print(os.path.dirname(os.path.abspath(__file__)))
