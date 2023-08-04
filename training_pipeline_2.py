178304781cedd09a18029508d816f0d5dc5b49c5
hf2503 <hfmole@hotmail.com>
2023-08-04 11:13:08 +0200

    test

--- training_pipeline_2.py
+++ training_pipeline_2.py
@@ -5,12 +5,14 @@ from model.utils import split_data_random, check_overlap
 TEST_SIZE = 0.3
 
 df = Data().data
+print(df['title'])
 train, test, title_dict = split_data_random(df)
 check_overlap(train, test)
-MovieModel().fit(df, 10)
+print(df.head)
+model_movie= MovieModel()
+model_movie.fit(df, 10)
 title = 'Star Wars: Episode IV - A New Hope (1977)'
 recommendations = MovieModel().evaluate(df, title, 10, title_dict)
-print(recommendations)
-MovieModel().stability(df, title, 10, title_dict)
-MovieModel().prediction_comparaison(df, ['Star Wars: Episode IV - A New Hope (1977)', 'Junior (1994)', 'Interview with the Vampire: The Vampire Chronicles (1994)'], 10, title_dict)
+model_movie.stability(df, title, 10, title_dict)
+model_movie.prediction_comparaison(df, ['Star Wars: Episode IV - A New Hope (1977)', 'Junior (1994)', 'Interview with the Vampire: The Vampire Chronicles (1994)'], 10, title_dict)
 print(recommendations)
