fc884f4e1038ebbcd9db7f88cf9f8821f242a7a2
hf2503 <97042427+hf2503@users.noreply.github.com>
2023-08-11 13:37:15 +0200

    test7

--- test_unitaires/data_test.py
+++ test_unitaires/data_test.py
@@ -8,28 +8,16 @@ parent_dir_path = os.path.abspath(os.path.join(dir_path,os.pardir))
 sys.path.insert(0,parent_dir_path)
 from utils.utils import make_directory
 from model_job.model_utils import *
-#from model_job.model.utils import *
-#from utils.path import input_data_folder, output_folder
 from utils.path import *
 from preprocessing_job.create_data import Data
-#print(get_last_final_csv(data_folder))
-#print(data_folder)
-#print(os.path.dirname(data_folder))
-#print(os.path.join((data_folder),'unittest/data_api.pkl'))
 
-#print(make_directory(unittest_folder))
-#print(unittest_folder)
-#print(os.path.join(unittest_folder,'test_data_final.pkl'))
 
-def get_data_final_pkl():
+def get_data_final_csv():
     file_path = os.path.join(data_unittest_folder,'test_data_final.pkl')
     with open(file_path,'rb') as df:
         return pickle.load(df)
 
-print(unittest_folder)
-print(data_unittest_folder)
-
-def get_data_api_pkl():
+def get_data_api_csv():
     file_path = os.path.join(data_unittest_folder,'test_api_data.pkl')
     with open(file_path,'rb') as df:
         return pickle.load(df)
@@ -44,46 +32,36 @@ def get_last_final_pkl(data_folder):
     last_final_pkl= pkl_files[0]
     return os.path.join(data_folder, last_final_pkl)
 
-print(get_last_final_pkl(unittest_folder))
-print(os.path.exists(unittest_folder))
 
 def test_existence_final_pkl():
     """check if the test_data_final exists"""
     assert get_last_final_pkl(data_unittest_folder) != None
 
 def test_existence_data_api_pkl():
-    """check if the file data_api.pkl exists"""
+    """check if the file data_api.pkl an data_final.pkl exists"""
     csv_files = [file for file in os.listdir(data_unittest_folder) if file.startswith("test") and file.endswith(".pkl")]
     assert len(csv_files) == 2
 
 def test_shape_final_csv():
     """check if final_*.csv is not empty """
-    #data_path = get_last_final_csv(data_folder)
-    df = get_data_final_pkl()
+    df = get_data_final_csv()
     assert df.empty == False
 
 def test_shape_data_api_csv():
     """check if data_api.csv is not empty """
-    #csv_files = [file for file in os.listdir(data_folder) if file.startswith("data_api") and file.endswith(".csv")]
-    #data_api_path = csv_files[0]
-    #df_api = pd.read_csv(data_folder+'/data_api.csv')
-    df_api = get_data_api_pkl()
+    df_api = get_data_api_csv()
     assert df_api.empty == False
+
 
 def test_nan_final_csv():
     """ check the Nan of the Dataframe final_*.csv"""
-    #data_path = get_last_final_csv(data_folder)
-    #df = pd.read_csv(data_path)
-    df_data_final = get_data_final_pkl()
+    df_data_final = get_data_final_csv()
     assert df_data_final.isna().sum().all() == False
 
 def test_type_final_csv():
     """check the variable type of the Dataframe final_*csv"""
-    #data_path = get_last_final_csv(data_folder)
-    df_test_type_final = get_data_final_pkl()
+    df_test_type_final = get_data_final_csv()
     assert \
         list(df_test_type_final.select_dtypes(include='int64').columns) == ['movieId', 'userId', 'timestamp', 'tagId'] \
         and\
-        list(df_test_type_final.select_dtypes(include='float64').columns) == ['rating', 'relevance']\
-        #and\
-        #list(df.select_dtypes(include='string').columns) == ['title','genres','tag','user_tag']
+        list(df_test_type_final.select_dtypes(include='float64').columns) == ['rating', 'relevance']
