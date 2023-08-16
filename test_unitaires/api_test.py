14110bd882ddc7486d316b55d485057a26ac88ac
hf2503 <hfmole@hotmail.com>
2023-08-17 00:46:55 +0200

    test_11

--- test_unitaires/api_test.py
+++ test_unitaires/api_test.py
@@ -18,6 +18,7 @@ def test_api_starting():
     """check if the API is running."""
     url = 'http://localhost:8000'
     response = requests.get(url)
+    
     assert response.status_code == 200
     assert response.json() == {"message": "API is up and running"}
 
