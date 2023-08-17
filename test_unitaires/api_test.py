d503e3db60be857f9906d8483e96f9cbc922aff2
hf2503 <hfmole@hotmail.com>
2023-08-17 23:27:06 +0200

    test

--- test_unitaires/api_test.py
+++ test_unitaires/api_test.py
@@ -22,7 +22,7 @@ import time
 
 def test_api_starting():
     """check if the API is running."""
-    url = 'http://172.30.192.1'
+    url = 'http://172.18.0.4'
     response = requests.get(url)
 
     assert response.status_code == 200
