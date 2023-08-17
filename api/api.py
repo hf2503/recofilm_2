730cfbc975567c87af9a4cf96bf574dc0fff734a
hf2503 <hfmole@hotmail.com>
2023-08-17 11:25:02 +0200

    test_10

--- api/api.py
+++ api/api.py
@@ -1,3 +1,15 @@
+#ajout pour les chemins
+import sys,os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir_path = os.path.abspath(os.path.join(dir_path,os.pardir))
+sys.path.insert(0,parent_dir_path)
+sys.path.insert(0,'../api')
+
+#fin ajout
+
+
+
+
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi import FastAPI, HTTPException, Response, status, Depends, Header, Query
 import json
