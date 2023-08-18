725508c60ba9431f55a7ea8d76a2310712afea8c
hf2503 <hfmole@hotmail.com>
2023-08-18 13:38:28 +0200

    test_

--- test_unitaires/api_test.py
+++ test_unitaires/api_test.py
@@ -4,26 +4,32 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(dir_path,os.pardir))
 sys.path.insert(0,parent_dir_path)
 
+import pandas as pd
 import pytest
 import requests
 from fastapi.testclient import TestClient
-#from api.api import app
+from api.api import app
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from requests.auth import HTTPBasicAuth
 import time
-#from api_utils.utils import *
 
-
-#data, movie_data, user_data, title_dict = get_data()
-
-#client = TestClient(app) compliqué
-
-#client = TestClient(app)
+client = TestClient(app)
 
 def test_api_starting():
     """check if the API is running."""
-    url = 'http://127.0.0.1:8000'
-    response = requests.get(url)
-
+    response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "API is up and running"}
+
+def test_api_unique_genre():
+    """check if the list of unique movies genres is not empty"""
+    response = client.get('/unique_genres')
+    assert response.status_code == 200
+    assert response.json() != None
+
+def test_api_get_random():
+    """check if the api gives always a random movie with an user known"""
+    response = client.get('/random?user_id=1644')
+    assert response.status_code == 200
+    assert response.json() != {'message': 'no movie for you:('}
+
