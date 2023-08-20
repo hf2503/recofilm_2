5478ff63e9962e2f548b7f6f20facf4168df6957
hf2503 <hfmole@hotmail.com>
2023-08-20 22:51:47 +0200

    test_git

--- api/api.py
+++ api/api.py
@@ -15,7 +15,7 @@ if os.getenv("GITHUB_ACTION") is None:
     data, movie_data, user_data, title_dict = get_data()
     GenreEnum = get_GenreEnum(data)
 else:
-    data = None
+    data = pd.read_csv(DATA_PATH)
     movie_data = None
     user_data = None
     title_dict = None
