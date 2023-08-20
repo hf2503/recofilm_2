9604bdb5caeeb8b5e9ac84fa52f8782804779a54
hf2503 <hfmole@hotmail.com>
2023-08-20 17:22:26 +0200

    test_

--- test_unitaires/api_test.py
+++ test_unitaires/api_test.py
@@ -7,45 +7,60 @@ parent_dir_path = os.path.abspath(os.path.join(dir_path,os.pardir))
 sys.path.insert(0,parent_dir_path)
 sys.path.append('../api')
 #fin ajout
-from scipy.sparse import csr_matrix
+
+from unittest.mock import MagicMock, call, mock_open, patch
 import pandas as pd
 import pytest
 import requests
-from fastapi import Depends
 from fastapi.testclient import TestClient
-import importlib
-
-# Reload the api module to ensure the patch takes effect
-from api.api import app
+from api.api import read_root,unique_genres,unique_movies,random_output,movie_model,user_model
+from api.api_utils.utils import query_params
+from fastapi import FastAPI, HTTPException, Response, status, Depends, Header, Query
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from requests.auth import HTTPBasicAuth
 import time
-from unittest.mock import Mock, patch
 
 
-data = pd.DataFrame({
-        'movieId': [1],
-        'rating': [3.5],
-        'userId': [1644],
-        'genres': ['Adventure|Animation|Children|Comedy|Fantasy'],
-        'title': ['Toy Story (1995)']
-    })
+BASE_URL = 'http://localhost:8000'
 
-client = TestClient(app)
+mock_data = pd.DataFrame([[1,3.5,1644,
+                           'Adventure|Animation|Children|Comedy|Fantasy','Toy Story (1995)']],
+                           index=['1'],
+                           columns=['movieId','rating','userId','genres','title'])
 
-
-def test_unique_genres():
-    with patch('api.api.data', data):
-        response = client.get("/unique_genres")
-        result = response.json()
-    expected_result = {'genres': ['Adventure', 'Animation','Children','Comedy', 'Fantasy']}
-
-    assert result == expected_result
+def test_api_starting(requests_mock):
+    requests_mock.get(f'{BASE_URL}/', json= {"message": "API is up and running"})
+    assert read_root() == requests.get(f'{BASE_URL}/').json()
 
 
-def test_unique_movies():
-    with patch('api.api.data', data):
-        response = client.get("/unique_movies")
-        result = response.json()
-    expected_result = {"movies":['Toy Story (1995)']}
-    assert result == expected_result
+def test_get_unique_genres(requests_mock):
+    with patch('api.api.data',mock_data):
+        result = unique_genres()
+    requests_mock.get(f'{BASE_URL}/unique_genres', json= {"genres": ["Adventure","Animation","Children",
+                                                                     "Comedy","Fantasy",]})
+    assert result == requests.get(f'{BASE_URL}/unique_genres').json()
 
+def test_get_unique_movies(requests_mock):
+
+    requests_mock.get(f'{BASE_URL}/unique_movies', json= {"movies":['Toy Story (1995)']})
+    with patch('api.api.data',mock_data):
+        result = unique_movies()
+    assert result == requests.get(f'{BASE_URL}/unique_movies').json()
+
+def test_get_random_output(requests_mock):
+    requests_mock.get(f'{BASE_URL}/random?user_id=1644',json= {'message': 'No movie for you :('})
+    with patch('api.api.data',mock_data):
+        result = random_output(query_params = {'user_id':'1644','subject':'','movie_name':''})
+    assert result == requests.get(f'{BASE_URL}/random?user_id=1644').json()
+
+def test_get_movie_model(requests_mock):
+    requests_mock.get(f'{BASE_URL}/movie_model?user_id=1644&movie_name=Toy%20Story%20%281995%294',json= {'message': 'No movie for you :('})
+    with patch('api.api.data',mock_data):
+        result = movie_model(query_params = {'user_id':'1644','subject':'','movie_name':'Toy Story (1995)'})
+        assert result == requests.get(f'{BASE_URL}/movie_model?user_id=1644&movie_name=Toy%20Story%20%281995%294').json()
+
+def test_get_user_model(requests_mock):
+    requests_mock.get(f'{BASE_URL}/user_model?user_id=1644',json= {'message': 'No movie for you :('})
+    with patch('api.api.data',mock_data):
+        result = user_model(query_params = {'user_id':'1644','subject':'','movie_name':'Toy Story (1995)'})
+        assert result == requests.get(f'{BASE_URL}/user_model?user_id=1644').json()
